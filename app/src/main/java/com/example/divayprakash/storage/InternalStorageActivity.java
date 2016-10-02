2c88558de0bf2dcc2830c7ca116ea210485db90c
Divay Prakash <divayprakash3@gmail.com>
2016-10-02 19:17:32 +0530

    Add InternalStorageActivity

---
+++ app/src/main/java/com/example/divayprakash/storage/InternalStorageActivity.java
@@ -0,0 +1,13 @@
+package com.example.divayprakash.storage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InternalStorageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_internal_storage);
+    }
+}
