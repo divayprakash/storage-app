6f65a6cbf78a6ab110fb626578748052cfc4bbe8
Divay Prakash <divayprakash3@gmail.com>
2016-10-02 22:59:32 +0530

    Add documentation to DatabaseActivity

--- app/src/main/java/com/example/divayprakash/storage/DatabaseActivity.java
+++ app/src/main/java/com/example/divayprakash/storage/DatabaseActivity.java
@@ -3,8 +3,18 @@ package com.example.divayprakash.storage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * The DatabaseActivity class implements all the control logic for the
+ * database storage demo.
+ *
+ * @author Divay Prakash
+ */
 public class DatabaseActivity extends AppCompatActivity {
 
+    /**
+     * This method is called at the startup of the application.
+     * @param savedInstanceState The saved instance state of the application.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
