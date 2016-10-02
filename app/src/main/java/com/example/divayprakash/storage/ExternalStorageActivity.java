5fcc45a155f8b4047f293cd23740c6fd388152e4
Divay Prakash <divayprakash3@gmail.com>
2016-10-02 19:18:15 +0530

    Add ExternalStorageActivity

---
+++ app/src/main/java/com/example/divayprakash/storage/ExternalStorageActivity.java
@@ -0,0 +1,13 @@
+package com.example.divayprakash.storage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExternalStorageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_external_storage);
+    }
+}
