43bfb34b1082bb1596a132de97afa247f83f9b65
Divay Prakash <divayprakash3@gmail.com>
2016-10-02 13:50:05 +0530

    Add SharedPreferencesActivity

---
+++ app/src/main/java/com/example/divayprakash/storage/SharedPreferencesActivity.java
@@ -0,0 +1,13 @@
+package com.example.divayprakash.storage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SharedPreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared_preferences);
+    }
+}
